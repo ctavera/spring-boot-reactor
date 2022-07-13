8df7fbae76d2fa67795d6c37e3b126e5f03d66d4
christian.tavera <christian.tavera@grupoargos.com.mx>
2022-07-13 16:14:21 -0500

    8. subscribe() Method.

--- src/main/java/da/springframework/springbootreactor/bootstrap/SpringBootReactorBootstrap.java
+++ src/main/java/da/springframework/springbootreactor/bootstrap/SpringBootReactorBootstrap.java
@@ -1,18 +1,25 @@
 package da.springframework.springbootreactor.bootstrap;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
+@Slf4j
 @Component
 public class SpringBootReactorBootstrap implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
         //Creates an Stream Flux Observable
-        Flux<String> names = Flux.just("Andrew", "Peter", "Derek", "Jhon")
-                .doOnNext(System.out::println); //name -> System.out.println(name)
+        Flux<String> names = Flux.just("Andrew", "Peter", "", "Derek", "Jhon")
+                .doOnNext(name -> {
+                    if (name.isEmpty()) {
+                        throw new RuntimeException("Nombres no pueden ser vacíos.");
+                    }
+                    System.out.println(name);
+                });
 
-        names.subscribe();
+        names.subscribe(name -> log.info(name), error -> log.error(error.getMessage()));
     }
 }
