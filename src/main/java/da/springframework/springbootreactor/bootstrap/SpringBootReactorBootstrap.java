96f19af4c77f6291d531b6d9c6ef2bba8f1690ce
christian.tavera <christian.tavera@grupoargos.com.mx>
2022-07-13 16:21:25 -0500

    9. onComplete event.

--- src/main/java/da/springframework/springbootreactor/bootstrap/SpringBootReactorBootstrap.java
+++ src/main/java/da/springframework/springbootreactor/bootstrap/SpringBootReactorBootstrap.java
@@ -12,7 +12,7 @@ public class SpringBootReactorBootstrap implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         //Creates an Stream Flux Observable
-        Flux<String> names = Flux.just("Andrew", "Peter", "", "Derek", "Jhon")
+        Flux<String> names = Flux.just("Andrew", "Peter", "Marie", "Derek", "Jhon")
                 .doOnNext(name -> {
                     if (name.isEmpty()) {
                         throw new RuntimeException("Nombres no pueden ser vacíos.");
@@ -20,6 +20,8 @@ public class SpringBootReactorBootstrap implements CommandLineRunner {
                     System.out.println(name);
                 });
 
-        names.subscribe(name -> log.info(name), error -> log.error(error.getMessage()));
+        names.subscribe(name -> log.info(name),
+                error -> log.error(error.getMessage()),
+                () -> log.info("Ha finalizado la ejecución del observable con éxito!")); //new Runnable() {@Override public void run() {...}});
     }
 }
