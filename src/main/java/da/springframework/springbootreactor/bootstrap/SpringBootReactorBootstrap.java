8277d515071c7b5de04b605d22074299e6f2dcea
christian.tavera <christian.tavera@grupoargos.com.mx>
2022-07-13 17:05:46 -0500

    10. Map operator.

--- src/main/java/da/springframework/springbootreactor/bootstrap/SpringBootReactorBootstrap.java
+++ src/main/java/da/springframework/springbootreactor/bootstrap/SpringBootReactorBootstrap.java
@@ -1,5 +1,6 @@
 package da.springframework.springbootreactor.bootstrap;
 
+import da.springframework.springbootreactor.model.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,15 +13,21 @@ public class SpringBootReactorBootstrap implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         //Creates an Stream Flux Observable
-        Flux<String> names = Flux.just("Andrew", "Peter", "Marie", "Derek", "Jhon")
-                .doOnNext(name -> {
-                    if (name.isEmpty()) {
+        Flux<User> names = Flux.just("Andrew", "Peter", "Marie", "Derek", "Jhon")
+                .map(name -> new User(name.toUpperCase(), null))
+                .doOnNext(user -> {
+                    if (user == null) {
                         throw new RuntimeException("Nombres no pueden ser vacíos.");
                     }
-                    System.out.println(name);
+                    System.out.println(user.getFirstName());
+                })
+                .map(user -> {
+                    String name = user.getFirstName().toLowerCase();
+                    user.setFirstName(name);
+                    return user;
                 });
 
-        names.subscribe(name -> log.info(name),
+        names.subscribe(user -> log.info(user.toString()),
                 error -> log.error(error.getMessage()),
                 () -> log.info("Ha finalizado la ejecución del observable con éxito!")); //new Runnable() {@Override public void run() {...}});
     }
---
+++ src/main/java/da/springframework/springbootreactor/model/User.java
@@ -0,0 +1,14 @@
+package da.springframework.springbootreactor.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+
+    private String firstName;
+    private String lastName;
+}
