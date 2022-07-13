6f00e5cbc1826fadb7cfc0ad12c7dc39fdaa2156
christian.tavera <christian.tavera@grupoargos.com.mx>
2022-07-13 13:39:13 -0500

    6. Creating proyect with Reactor API. Initial commit

---
+++ src/test/java/da/springframework/springbootreactor/SpringBootReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package da.springframework.springbootreactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
