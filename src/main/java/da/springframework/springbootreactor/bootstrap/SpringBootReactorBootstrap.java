68b67d9beac13df65ee7615adb97f64d149f6e55
christian.tavera <christian.tavera@grupoargos.com.mx>
2022-07-14 09:47:21 -0500

    13. Creating a Flux (Observable) from a List or Iterable.

--- src/main/java/da/springframework/springbootreactor/bootstrap/SpringBootReactorBootstrap.java
+++ src/main/java/da/springframework/springbootreactor/bootstrap/SpringBootReactorBootstrap.java
@@ -6,14 +6,28 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 @Component
 public class SpringBootReactorBootstrap implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        List<String> userNames = new ArrayList<>();
+        userNames.add("Andrew Garfield");
+        userNames.add("Peter Parker");
+        userNames.add("Marie Curie");
+        userNames.add("Derek Smith");
+        userNames.add("Jhon Doe");
+        userNames.add("Bruce Lee");
+        userNames.add("Bruce Willis");
+
         //Creates an Stream Flux Observable
-        Flux<String> names = Flux.just("Andrew Garfield", "Peter Parker", "Marie Curie", "Derek Smith", "Jhon Doe", "Bruce Lee", "Bruce Willis");
+//        Flux<String> names = Flux.just("Andrew Garfield", "Peter Parker", "Marie Curie", "Derek Smith", "Jhon Doe", "Bruce Lee", "Bruce Willis");
+        Flux<String> names = Flux.fromIterable(userNames);
 
         Flux<User> users = names.map(name -> new User(name.split(" ")[0].toUpperCase(), name.split(" ")[1].toUpperCase()))
                 .filter(user -> user.getFirstName().toLowerCase().equals("bruce"))
